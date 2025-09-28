8ad6da3e48e86f978d86f17eff7e523040e53b99
Karthik-S-Salian <karthikssalian5@gmail.com>
2025-09-28 19:48:24 +0530

    better configs

--- commands.sh
+++ commands.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-gnome-terminal -- bash -c "/usr/share/logstash/bin/logstash -f /home/karthikssalian/work/hpe/logstash/logstash.conf --path.settings /etc/logstash --path.data /home/karthikssalian/work/hpe/logstash/data/ --path.logs /home/karthikssalian/work/hpe/logstash/logs/; exec bash"
+gnome-terminal -- bash -c "./app/logstash-9.1.2/bin/logstash -f ./logstash/logstash.conf --path.settings ./app/logstash-9.1.2/config/logstash.yml  --path.data ./logstash/data/ --path.logs ./logstash/logs/; exec bash"
 
-gnome-terminal -- bash -c "/usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties; exec bash"
+gnome-terminal -- bash -c "./app/kafka/bin/kafka-server-start.sh ./app/kafka/config/server.properties; exec bash"
 
-gnome-terminal -- bash -c "/opt/fluent-bit/bin/fluent-bit -c ~/work/hpe/fluentbit/fluentbit.conf; exec bash"
+gnome-terminal -- bash -c "fluent-bit -c ./fluentbit/fluentbit.conf; exec bash"
 
-gnome-terminal -- bash -c "/bin/python3 /home/karthikssalian/work/hpe/generators/fake_logs.py; exec bash"
+gnome-terminal -- bash -c "/bin/python3 ./generators/fake_logs.py; exec bash"
 
-gnome-terminal -- bash -c "./victoria-logs-prod -storageDataPath=victoria-logs-data"
+gnome-terminal -- bash -c "./app/victoria-logs-prod -storageDataPath=victoria-logs-data"
+
+
+./app/logstash-9.1.2/bin/logstash \
+  -f /home/karthikssalian/work/hpe-cty-opentelemetry/logstash/logstash.conf \
+  --path.settings /home/karthikssalian/work/hpe-cty-opentelemetry/app/logstash-9.1.2/config \
+  --path.data ./logstash/data \
+  --path.logs ./logstash/logs
